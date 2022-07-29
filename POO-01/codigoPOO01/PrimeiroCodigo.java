9f684a0476456bd1e026e2599795cbc109f33dfc
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-29 12:16:30 -0300

    Update PrimeiroCodigo.java

--- POO-01/codigoPOO01/PrimeiroCodigo.java
+++ POO-01/codigoPOO01/PrimeiroCodigo.java
@@ -1,6 +1,7 @@
 public class PrimeiroCodigo {
     public static void main(String[] args) {
         System.out.println("Imprime agora");
+        System.out.println("Digige o Primeiro Numero");
         
     }
     
