60a3f9438079d243296a8cb940201fb501f8a1d5
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-04 20:05:33 -0300

    Update

--- POO-01/codigoPOO01/PrimeiroCodigo.java
+++ POO-01/codigoPOO01/PrimeiroCodigo.java
@@ -2,6 +2,8 @@ public class PrimeiroCodigo {
     public static void main(String[] args) {
         System.out.println("Imprime agora");
         System.out.println("Digige o Primeiro Numero");
+        System.out.println("Bom dia !");
+        System.out.printf("Agora !");
         
     }
     
---
+++ extrutu01/estrutura01/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    
+    public static void main(String[] args) {
+        
+        double x = 10.12345;
+
+        System.out.printf("%.2f", (x));
+    }
+
+
+}
+
