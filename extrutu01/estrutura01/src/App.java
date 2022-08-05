9f3d3bba4acb58419b7abfad75668049a42d7c39
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-04 21:25:02 -0300

    Update

--- extrutu01/estrutura01/src/App.java
+++ extrutu01/estrutura01/src/App.java
@@ -3,10 +3,16 @@ public class App {
     public static void main(String[] args) {
         
         double x = 10.12345;
+        double y = 5.5;
 
-        System.out.printf("%.2f", (x));
+        String nome = "Maria";
+        int idade = 31;
+        float renda = 4000.0f;
+
+        System.out.printf("%.2f%n", (x));
+        System.out.printf("%s tem %d de idade e possui renda de RS %.2f reais ", nome, idade, renda);
+        
     }
 
 
 }
-
