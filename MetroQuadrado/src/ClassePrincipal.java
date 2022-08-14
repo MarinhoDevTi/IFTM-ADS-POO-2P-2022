0c0dadf1dfc5939a689d7da1ae200075692e93c2
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-13 21:24:28 -0300

    Update

---
+++ MetroQuadrado/src/ClassePrincipal.java
@@ -0,0 +1,13 @@
+public class ClassePrincipal {
+    public static void main(String[] args) {
+
+        //inicio do programa...
+        System.out.println("-=-=-=-=-= .:: Calculo de metros quadrados ::. -=-=-=-=-=-");
+        System.out.println("-----------------------------------------------------------");
+
+        //leitura de dados ...
+        System.out.println("Digite a largura do terreno: ");
+                
+    }
+    
+}
