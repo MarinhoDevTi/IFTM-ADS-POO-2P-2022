02a7ee0192595c7566f45bfe70675cfa0d8a5b36
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-14 17:13:41 -0300

    Update ClassePrincipal.java

--- retangulo/src/ClassePrincipal.java
+++ retangulo/src/ClassePrincipal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ClassePrincipal {
     public static void main(String[] args) {
+        
+        /*Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida,
+    mostrar o valor da área, perímetro e diagonal deste retângulo, com quatro casas
+    decimais, conforme exemplos.*/
+        
+        //declaração metodo Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //declaração de variavies
+        double base, altura, area, perimetro, diagonal;
+
+        System.out.println("-=-=-= .:: Altura, Perimetro e Diagonal ::. -=-=-");
+        System.out.println("=================================================");
+
+        //leitura de dados ...
+        System.out.println("Digite a base do retângulo: ");
+        base = sc.nextDouble();
+        System.out.println("________________________________________");
+
+        System.out.println("Digite a altura do retângulo:");
+        altura = sc.nextDouble();
+        System.out.println("________________________________________");
+
+        System.out.println("=================================================");
+
+        
+
+
+        
+
         
     }
 }
