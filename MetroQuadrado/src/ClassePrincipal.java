ca0653edbd4c342ae5c140fd4e168915ca4e7260
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-13 22:08:51 -0300

    Update

--- MetroQuadrado/src/ClassePrincipal.java
+++ MetroQuadrado/src/ClassePrincipal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class ClassePrincipal {
     public static void main(String[] args) {
+
+        //variavel scanner
+        Scanner sc = new Scanner(System.in);
+
+
+
+        //declaração de variaveis ...
+        double largTer;
+        double compriTer;
+        double varMet;
+        double met2;
+        double precTot;
+
 
         //inicio do programa...
         System.out.println("-=-=-=-=-= .:: Calculo de metros quadrados ::. -=-=-=-=-=-");
@@ -7,6 +21,26 @@ public class ClassePrincipal {
 
         //leitura de dados ...
         System.out.println("Digite a largura do terreno: ");
+        largTer = sc.nextDouble();
+        System.out.println("------------------------------");
+
+        System.out.println("Digite o comprimento do trerreno: ");
+        compriTer = sc.nextDouble();
+        System.out.println("------------------------------");
+        
+
+        System.out.println("Digite o valor do metro quadrado: ");
+        varMet = sc.nextDouble();
+        System.out.println("------------------------------");
+
+        //processamento de dados
+        met2 = largTer * compriTer;
+        precTot = met2 * varMet; 
+
+
+
+        //saida de dados ...
+            System.out.printf("Valor total do metro quadrado: %.2 " + precTot);
                 
     }
     
