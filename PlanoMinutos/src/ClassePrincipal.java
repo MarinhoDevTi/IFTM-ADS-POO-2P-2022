465b7d1a0946ea10d771fe29399301c6af9a9852
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-18 20:32:23 -0300

    Update

---
+++ PlanoMinutos/src/ClassePrincipal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ClassePrincipal {
+    public static void main(String[] args) {
+        
+        //variável de leitura...
+        Scanner sc = new Scanner(System.in);
+
+        //declaração de variaveis
+        double minutos, valor, valori = 50.0;
+
+        System.out.println("=-=-=-=- .::: Calcula Plano de telefonia :::. =-=-=-=-");
+        System.out.println("======================================================");
+
+
+        //leitura de dados
+        System.out.println("Minutos gastos do cliente: ");
+        minutos = sc.nextDouble();
+
+        //tratamento de condições 
+        if (minutos <= 100 ) {
+            valor = valori;
+            System.out.println("Este é o valor da fatura: R$ " + valor);
+
+        } else {
+            valor = valori + (minutos - 100)  * 2;
+            System.out.println("Este é o valor da fatura: R$ " + valor);
+        }  
+        sc.close();
+
+    }
+    
+    
+}
