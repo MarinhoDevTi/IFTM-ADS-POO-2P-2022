85f28ddf8a157cf217e7c87749f3c9d15a617519
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-18 21:20:19 -0300

    Update ClassePrincipal.java

--- PlanoMinutos/src/ClassePrincipal.java
+++ PlanoMinutos/src/ClassePrincipal.java
@@ -7,7 +7,7 @@ public class ClassePrincipal {
         Scanner sc = new Scanner(System.in);
 
         //declaração de variaveis
-        double minutos, valor, valori = 50.0;
+        double minutos, valor, valori = 50.0, taxam = 15.0;
 
         System.out.println("=-=-=-=- .::: Calcula Plano de telefonia :::. =-=-=-=-");
         System.out.println("======================================================");
@@ -18,17 +18,25 @@ public class ClassePrincipal {
         minutos = sc.nextDouble();
 
         //tratamento de condições 
-        if (minutos <= 100 ) {
+        if (minutos <= 0) {
+
+            System.out.println("cliente gastou quantidade mínima: R$ " + taxam);
+            
+        } else if (minutos <= 100){
+
             valor = valori;
-            System.out.println("Este é o valor da fatura: R$ " + valor);
+            System.out.println("Este é o valor da fatura: R$ " + valor);        
 
         } else {
+
             valor = valori + (minutos - 100)  * 2;
             System.out.println("Este é o valor da fatura: R$ " + valor);
-        }  
+        } 
         sc.close();
 
     }
-    
+    //
+    //
+    //
     
 }
