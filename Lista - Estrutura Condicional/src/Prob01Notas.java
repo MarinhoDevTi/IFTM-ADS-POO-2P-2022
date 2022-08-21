23a5593b4bc095261891e604b7579be5b5f7d82d
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-21 18:44:07 -0300

    Update Prob01Notas.java

--- Lista - Estrutura Condicional/src/Prob01Notas.java
+++ Lista - Estrutura Condicional/src/Prob01Notas.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Prob01Notas {
     public static void main(String[] args) {
         
+        //Declaração de variável de leitura 
+        Scanner ltr = new Scanner(System.in);
+
+        //declação de variaveis 
+        double notas01, notas02, notafim;
+
+        System.out.println("-=-=-= .:: Lançamento de Notas :;. =-=-=-");
+        
+        //leitura de dados
+        System.out.println("Notas do Primeiro semestre: []");
+        notas01 = ltr.nextDouble();
+
+        System.out.println("Notas do Segundo semestre: []");
+        notas02 = ltr.nextDouble();
+
+        //processamento e saida de dados
+        notafim = notas01 + notas02;
+
+        if (notafim > 60) {
+            System.out.printf("Notas totais do Aluno: %.f1", notafim);
+            System.out.println("Parabéns você foi aprovado");
+        } else {
+            System.out.printf("Notas totais do Aluno: %.f1", notafim);
+            System.out.println("Aluno REPROVADO");
+        }
+
+
     }
     
 }
