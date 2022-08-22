1b4a87efb4c90b550408f369efc9dfb995f00fa0
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-21 21:42:26 -0300

    Update Prob02Baskara.java

--- Lista - Estrutura Condicional/src/Prob02Baskara.java
+++ Lista - Estrutura Condicional/src/Prob02Baskara.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Prob02Baskara {
     public static void main(String[] args) {
+
+        //variavel de entrada
+        Scanner ltr = new Scanner(System.in);
+
+        //declaração de variaveis
+        int a, b, c;
+        double x1, x2, delta;
+
+        System.out.println("+====================================+");
+        System.out.println("-=-=-=-= .::: Fórmula de Bhaskara :::. =-=-=-=-");
+
+        
         
     }
 }
