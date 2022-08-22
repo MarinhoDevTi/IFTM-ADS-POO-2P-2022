fa2a14f05936be5fd3d83cbfd6bf4705cc7eb4f5
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-08-21 22:37:47 -0300

    Update Prob02Baskara.java

--- Lista - Estrutura Condicional/src/Prob02Baskara.java
+++ Lista - Estrutura Condicional/src/Prob02Baskara.java
@@ -13,7 +13,30 @@ public class Prob02Baskara {
         System.out.println("+====================================+");
         System.out.println("-=-=-=-= .::: Fórmula de Bhaskara :::. =-=-=-=-");
 
-        
-        
+        //leitura de dados 
+        System.out.println("Digite os valores de [a] [b] [c]");
+        a = ltr.nextInt();
+        b = ltr.nextInt();
+        c = ltr.nextInt();
+
+        //cálculo ( valor de delta )
+        delta = Math.pow(b,2)- 4 * a * c;
+
+        //valores de X' e X"
+        x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+        //saida dos valores
+        System.out.println("+====================================+");
+        System.out.println("Este é o valor de Delta: " + delta);
+        System.out.println("+====================================+");
+        System.out.println("Este é o valor do X1: " + x1);
+        System.out.println("+====================================+");
+        System.out.println("Este é o valor do X2 " + x2);
+        System.out.println("+====================================+");
+
+        ltr.close();
     }
+    
+
 }
