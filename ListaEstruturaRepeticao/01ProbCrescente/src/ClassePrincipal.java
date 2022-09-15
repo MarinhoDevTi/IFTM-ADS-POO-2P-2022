2a31c63cdb16e92fbe594ea54ee646d1aabbb68c
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-09-14 22:04:37 -0300

    Update ClassePrincipal.java

--- ListaEstruturaRepeticao/01ProbCrescente/src/ClassePrincipal.java
+++ ListaEstruturaRepeticao/01ProbCrescente/src/ClassePrincipal.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class ClassePrincipal {
     public static void main(String[] args) {
+
+        /* 
+         * 1. Problema “crescente”
+
+        Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para
+        cada X e Y uma mensagem que indique se estes valores foram digitados em ordem
+        crescente ou decrescente. O programa deve finalizar quando forem digitados dois
+        valores iguais.
+        */
+
+        //declaração variavel de Leitura
+        Scanner ltr = new Scanner(System.in);
+
+        System.out.println("Digite o Primeiro número: ");
+        
+
         
     }
-    }
+    
 }
